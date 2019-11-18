d2b8edd96ae26c84afd3349324b513cbf7a0ddf3
Joseph Ivie <josephivie@gmail.com>
2019-11-18 11:57:44 -0700

    Multiple types in recycler view

---
+++ ios/Kwift Template/swiftResources/layouts/MultipleDemoXml.swift
@@ -0,0 +1,52 @@
+//
+// MultipleDemoXml.swift
+// Created by Kwift XML
+//
+
+import UIKit
+import Kwift
+import MapKit
+
+public class MultipleDemoXml {
+    
+    public unowned var xmlRoot: UIView!
+    public func setup(_ dependency: ViewDependency) -> UIView {
+        let view = LinearLayout(frame: .zero)
+        view.orientation = .y
+        view.padding = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        view.gravity = .topLeft
+        
+        view.addSubview(
+            UILabel(frame: .zero),
+            minimumSize: CGSize(width: 0, height: 0),
+            size: CGSize(width: 0, height: 0),
+            margin: UIEdgeInsets(top: 16 + 0, left: 16 + 0, bottom: 16 + 0, right: 16 + 0),
+            gravity: .topFill,
+            weight: 0
+        ) { view in 
+            view.text = ResourcesStrings.checkOutMyMultipleRows
+            view.numberOfLines = 0
+            view.font = UIFont.get(size: 24, style: ["bold"])
+            view.textColor = UIColor(argb: 0xFF000000)
+            view.textAlignment = .center
+            view.baselineAdjustment = .alignCenters
+        }
+        
+        view.addSubview(
+            UITableView(frame: .zero),
+            minimumSize: CGSize(width: 0, height: 0),
+            size: CGSize(width: 0, height: 0),
+            margin: UIEdgeInsets(top: 0 + 0, left: 0 + 0, bottom: 0 + 0, right: 0 + 0),
+            gravity: .topFill,
+            weight: 1.0
+        ) { view in 
+            self.list = view
+        }
+        
+        xmlRoot = view
+        return view
+    }
+    
+    public unowned var list: UITableView!
+    
+}
